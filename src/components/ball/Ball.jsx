2ee9ba721c92c5dcb09f0544b012dc3b075d9bb4
Ahmet Aydin <birceste@gmail.com>
2022-10-28 08:23:21 +0300

    fix

--- src/components/ball/Ball.jsx
+++ src/components/ball/Ball.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 const Ball = ({ turn, ball }) => {
   const [color, setColor] = useState(turn);
+  setColor(turn);
   console.log(turn);
   return color ? (
     <img src={RedBall} alt="redBall" />
