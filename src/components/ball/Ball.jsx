3e735c8787eeb2f997896b970b9701f41c6d2f22
Ahmet Aydin <birceste@gmail.com>
2022-10-28 08:25:56 +0300

    hey

--- src/components/ball/Ball.jsx
+++ src/components/ball/Ball.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import RedBall from "../../assets/images/counter-red-large.svg";
 import YellowBall from "../../assets/images/counter-yellow-large.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Ball = ({ turn, ball }) => {
   const [color, setColor] = useState(turn);
-  setColor(turn);
+  useEffect(() => {
+    setColor(turn);
+  }, []);
+
   console.log(turn);
   return color ? (
     <img src={RedBall} alt="redBall" />
