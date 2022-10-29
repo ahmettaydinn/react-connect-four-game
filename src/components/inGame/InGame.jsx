b05c4a6745061f682b1ddc40ba41827df55045e1
Ahmet Aydin <birceste@gmail.com>
2022-10-29 05:11:23 +0300

    winner algorithm created

--- src/components/inGame/InGame.jsx
+++ src/components/inGame/InGame.jsx
@@ -17,6 +17,78 @@ import YellowMarker from "../../assets/images/marker-yellow.svg";
 import Styles from "./InGame.module.scss";
 
 const InGame = () => {
+  let winningArray = [
+    [0, 1, 2, 3],
+    [41, 40, 39, 38],
+    [7, 8, 9, 10],
+    [34, 33, 32, 31],
+    [14, 15, 16, 17],
+    [27, 26, 25, 24],
+    [21, 22, 23, 24],
+    [20, 19, 18, 17],
+    [28, 29, 30, 31],
+    [13, 12, 11, 10],
+    [35, 36, 37, 38],
+    [6, 5, 4, 3],
+    [0, 7, 14, 21],
+    [41, 34, 27, 20],
+    [1, 8, 15, 22],
+    [40, 33, 26, 19],
+    [2, 9, 16, 23],
+    [39, 32, 25, 18],
+    [3, 10, 17, 24],
+    [38, 31, 24, 17],
+    [4, 11, 18, 25],
+    [37, 30, 23, 16],
+    [5, 12, 19, 26],
+    [36, 29, 22, 15],
+    [6, 13, 20, 27],
+    [35, 28, 21, 14],
+    [0, 8, 16, 24],
+    [41, 33, 25, 17],
+    [7, 15, 23, 31],
+    [34, 26, 18, 10],
+    [14, 22, 30, 38],
+    [27, 19, 11, 3],
+    [35, 29, 23, 17],
+    [6, 12, 18, 24],
+    [28, 22, 16, 10],
+    [13, 19, 25, 31],
+    [21, 15, 9, 3],
+    [20, 26, 32, 38],
+    [36, 30, 24, 18],
+    [5, 11, 17, 23],
+    [37, 31, 25, 19],
+    [4, 10, 16, 22],
+    [2, 10, 18, 26],
+    [39, 31, 23, 15],
+    [1, 9, 17, 25],
+    [40, 32, 24, 16],
+    [9, 7, 25, 33],
+    [8, 16, 24, 32],
+    [11, 7, 23, 29],
+    [12, 18, 24, 30],
+    [1, 2, 3, 4],
+    [5, 4, 3, 2],
+    [8, 9, 10, 11],
+    [12, 11, 10, 9],
+    [15, 16, 17, 18],
+    [19, 18, 17, 16],
+    [22, 23, 24, 25],
+    [26, 25, 24, 23],
+    [29, 30, 31, 32],
+    [33, 32, 31, 30],
+    [36, 37, 38, 39],
+    [40, 39, 38, 37],
+    [7, 14, 21, 28],
+    [8, 15, 22, 29],
+    [9, 16, 23, 30],
+    [10, 17, 24, 31],
+    [11, 18, 25, 32],
+    [12, 19, 26, 33],
+    [13, 20, 27, 34],
+  ];
+
   const boardRef = useRef();
 
   const [redPlayerBalls, setRedPlayerBalls] = useState([]);
@@ -39,6 +111,34 @@ const InGame = () => {
   const [balls, setBalls] = useState([]);
   const [boardPosition, setBoardPosition] = useState({ x: 0, y: 0 });
   const [timer, setTimer] = useState(0);
+  const [winner, setWinner] = useState("");
+
+  useEffect(() => {
+    let redCounter = 0;
+    let yellowCounter = 0;
+    for (let i = 0; i < winningArray.length; i++) {
+      for (let j = 0; j < winningArray[i].length; j++) {
+        if (redPlayerBalls.includes(winningArray[i][j])) {
+          redCounter++;
+        } else if (yellowPlayerBalls.includes(winningArray[i][j])) {
+          yellowCounter++;
+        }
+        if (redCounter > 3) {
+          setWinner("yellow");
+          console.log(winningArray[i]);
+        }
+
+        if (yellowCounter > 3) {
+          setWinner("red");
+          console.log(winningArray[i]);
+        }
+      }
+      redCounter = 0;
+      yellowCounter = 0;
+    }
+
+    console.log(winner);
+  }, [turn]);
 
   useEffect(() => {
     setTimeout(() => {
@@ -157,10 +257,6 @@ const InGame = () => {
 
       setSlot({ ...slot, [column]: slot[column] - 1 });
       setTurn(!turn);
-      console.log("red side");
-      console.log(redPlayerBalls);
-      console.log("yellow side");
-      console.log(yellowPlayerBalls);
     }
   };
 
@@ -170,6 +266,7 @@ const InGame = () => {
         <p>{`x: ${markerMove.x} , y: ${markerMove.y}`}</p>
         <p>{`column: ${column} row: ${row}`}</p>
         <p>{`board positionX: ${boardPosition.x} , board positionY: ${boardPosition.y}`}</p>
+        <p>{winner} is the winner</p>
       </div>
 
       <nav className={Styles.inGameNav}>
